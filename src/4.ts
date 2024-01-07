7bbde8f28c3747029cfa2a17135f94c552a7d559
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-01-07 16:18:51 +0200

    4th task

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,41 @@
+class Key {
+  private readonly signature: number = Math.random();
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public readonly tenants: Person[] = [];
+  door: boolean;
+  key: Key;
+  comeIn(guest: Person): void {
+    if (this.door) {
+      this.tenants.push(guest);
+    }
+  }
+  abstract openDoor(myKey: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +44,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
